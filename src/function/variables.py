2fe1ec90d9daf62e4f7137bec7a77fe2edbca687
kal <dilipkalagotla@gmail.com>
2021-11-02 09:27:44 -0400

    Added comments & TODO to variables and it's test

--- src/function/variables.py
+++ src/function/variables.py
@@ -1,10 +1,55 @@
 # Calculates different vairables from plot3d data
 # Equations can be found here: https://www.grc.nasa.gov/WWW/winddocs/towne/plotc/plotc_p3d.html
-
+# TODO: Add more variables
+#  Enthalpies, Vorticity, Entropy, Turbulence Parameters, Gradients, Move metrics here?
+#  Total quantities
 import numpy as np
 
 
 class Variables:
+    """
+    Module to compute flow variables
+
+    ...
+
+    Attributes
+    ----------
+    Input:
+        flow : src.io.plot3dio.FlowIO or src.streamlines.interpolation.Interpolation or similar object
+            object with q --> flow data
+        gamma : float
+            default is 1.4; can specify
+    Output:
+        velocity : ndarray
+            velocity of the flow at nodes; shape of the flow.q (ni x nj x nk x 3)
+        velocity_magnitude : ndarray
+            magnitude of velocity at nodes; shape is (ni x nj x nk)
+        temperature : ndarray
+            temperature at nodes; shape is (ni x nj x nk)
+        pressure : ndarray
+            pressure at nodes; shape is (ni x nj x nk)
+
+    Methods
+    -------
+    compute_velocity()
+        computes the velocity and velocity_magnitude
+    compute_temperature()
+        computes the temperature
+    compute_pressure()
+        computes the pressure
+    compute()
+        computes all the variables available
+
+    ...
+
+    Example:
+    -------
+        variables = Variables(flow)  # Assume flow object is pre-defined
+        variables.compute_velocity()  # returns velocity attribute
+        variables.compute_temperature()  # fills up the temperature attribute
+        variables.compute()  # computes all the attributes available
+
+    """
 
     def __init__(self, flow, gamma=1.4):
         self.flow = flow
@@ -15,18 +60,37 @@ class Variables:
         self.pressure = None
 
     def compute_velocity(self):
+        """
+        Function to compute velocity and velocity magnitude
+        :return: None
+        """
         self.velocity = self.flow.q[..., 1:4] / (self.flow.q[..., 0, None] * self.flow.mach)
         self.velocity_magnitude = (self.velocity[..., 0]**2 + self.velocity[..., 1]**2 + self.velocity[..., 2]**2)**0.5
 
     def compute_temperature(self):
+        """
+        Function to compute temperature.
+        This computes velocity first
+        :return: None
+        """
         self.compute_velocity()
         _R = 1 / (self.gamma * self.flow.mach**2)
         _E_T = self.flow.q[..., 4] / self.flow.mach**2
         self.temperature = (self.gamma - 1) * (_E_T/self.flow.q[..., 0] - self.velocity_magnitude/2) / _R
 
     def compute_pressure(self):
+        """
+        Function to compute pressure.
+        This computes velocity and temperature first
+        :return: None
+        """
         self.compute_temperature()
         self.pressure = self.flow.q[..., 0] * self.temperature
 
     def compute(self):
-        self.compute_pressure()  # implicitly runs compute_velocity and compute_temperature()
+        # implicitly runs compute_velocity() and compute_temperature()
+        """
+        Function to compute all the attributes in the class
+        :return: None
+        """
+        self.compute_pressure()
--- test/test_variables.py
+++ test/test_variables.py
@@ -6,6 +6,7 @@ class TestVariables(unittest.TestCase):
     from src.streamlines.search import Search
     from src.streamlines.interpolation import Interpolation
 
+    # Get the data at a given point
     grid = GridIO('../data/plate_data/plate.sp.x')
     flow = FlowIO('../data/plate_data/sol-0000010.q')
     idx = Search(grid, [8.5, 0.5, 0.01])
@@ -17,6 +18,12 @@ class TestVariables(unittest.TestCase):
     point_data.compute()
 
     def test_variables(self, flow=flow):
+        """
+        To test variables class for the whole domain
+        Test each function separately
+        :param flow: FlowIO object
+        :return: None
+        """
         from src.function.variables import Variables
 
         variables = Variables(flow)
@@ -27,6 +34,11 @@ class TestVariables(unittest.TestCase):
         self.assertEqual(variables.temperature.shape, (720, 152, 129))
 
     def test_variables_compute(self, flow=flow):
+        """
+        Test "compute" method in variables class
+        :param flow: FlowIO object
+        :return: None
+        """
         from src.function.variables import Variables
 
         variables = Variables(flow)
@@ -39,7 +51,7 @@ class TestVariables(unittest.TestCase):
         """
         Inside function to test point_variables
 
-        :param point_variables: src.function.variables.Variables
+        :param point_variables: Interpolation object
         :return: None
         """
 
@@ -49,6 +61,11 @@ class TestVariables(unittest.TestCase):
         self.assertEqual(abs(point_variables.temperature - 1.0003078917225714) <= 1e-6, True)
 
     def test_point_variables(self, point_data=point_data):
+        """
+        Test variables class for a single point
+        :param point_data: Interpolation object
+        :return: None
+        """
         from src.function.variables import Variables
 
         point_variables = Variables(point_data)
@@ -58,6 +75,11 @@ class TestVariables(unittest.TestCase):
         self._test(point_variables)
 
     def test_point_variables_compute(self, point_data=point_data):
+        """
+        Test "compute" method in variables class for a single point
+        :param point_data: Interpolation object
+        :return: None
+        """
         from src.function.variables import Variables
 
         point_variables = Variables(point_data)
