b34f54939621c4985403e19079b6e69dda37e7ed
kal <dilipkalagotla@gmail.com>
2022-01-21 13:24:16 -0500

    Added cpoint and ppoint to keep track simultaneously in both p-space and c-space. Condensed pRK4 and cRK4 for better readability. All the in-domain check is relegated to search algos

--- src/streamlines/integration.py
+++ src/streamlines/integration.py
@@ -6,24 +6,26 @@ class Integration:
 
     def __init__(self, interp):
         self.interp = interp
-        self.point = None
+        self.ppoint = None
+        self.cpoint = None
 
     def compute(self, method='p-space', time_step=1e-6):
         from src.function.variables import Variables
         # TODO add switch case
         if method == 'p-space':
             # For p-space algos; the point -in-domain check was done in search
-            if self.interp.idx.point is None:
-                self.point = None
-                return self.point
+            if self.interp.idx.ppoint is None:
+                self.ppoint = None
+                return self.ppoint
 
             q_interp = Variables(self.interp)
             q_interp.compute_velocity()
-            self.point = self.interp.idx.point + q_interp.velocity.reshape(3) * time_step
+            self.ppoint = self.interp.idx.ppoint + q_interp.velocity.reshape(3) * time_step
 
-            return self.point
+            return self.ppoint
 
         if method == 'c-space':
+            # Get inverse Jacobian from the interpolation class
             _J_inv = self.interp.idx.grid.m2[self.interp.idx.cell[0, 0], self.interp.idx.cell[0, 1],
                                              self.interp.idx.cell[0, 2], :, :, self.interp.idx.block]
 
@@ -31,76 +33,60 @@ class Integration:
             q_interp.compute_velocity()
             p_velocity = q_interp.velocity.reshape(3)
             c_velocity = np.matmul(_J_inv, p_velocity)
-            self.point = self.interp.idx.point + c_velocity * time_step
+            self.cpoint = self.interp.idx.cpoint + c_velocity * time_step
 
             # For c-spce the point in-domain check is done after integration
-            if not np.all([0, 0, 0] <= self.point) or not np.all(
-                    self.point + 1 < [self.interp.idx.grid.ni[self.interp.idx.block],
-                                      self.interp.idx.grid.nj[self.interp.idx.block],
-                                      self.interp.idx.grid.nk[self.interp.idx.block]]):
-                self.point = None
-                return self.point
+            if not np.all([0, 0, 0] <= self.cpoint) or not np.all(
+                    self.cpoint + 1 < [self.interp.idx.grid.ni[self.interp.idx.block],
+                                       self.interp.idx.grid.nj[self.interp.idx.block],
+                                       self.interp.idx.grid.nk[self.interp.idx.block]]):
+                self.cpoint = None
+                return self.cpoint
 
-            return self.point
+            return self.cpoint
 
         if method == 'pRK4':
             from src.streamlines.interpolation import Interpolation
             from src.streamlines.search import Search
 
+            def _rk4_step(self, x):
+                idx = Search(self.interp.idx.grid, x)
+                idx.compute()
+                # For p-space algos; the point-in-domain check was done in search
+                if idx.ppoint is None: return None
+                interp = Interpolation(self.interp.flow, idx)
+                interp.compute()
+                q_interp = Variables(interp)
+                q_interp.compute_velocity()
+                v = q_interp.velocity.reshape(3)
+                k = time_step * v
+                return k
+
             # Start RK4 for p-space
             # For p-space algos; the point-in-domain check was done in search
-            x0 = self.interp.idx.point
-            if self.interp.idx.point is None:
-                self.point = None
-                return self.point
+            x0 = self.interp.idx.ppoint
+            if x0 is None: return None
             q_interp = Variables(self.interp)
             q_interp.compute_velocity()
             v0 = q_interp.velocity.reshape(3)
             k0 = time_step * v0
             x1 = x0 + 0.5 * k0
 
-            idx = Search(self.interp.idx.grid, x1)
-            idx.compute()
-            interp = Interpolation(self.interp.flow, idx)
-            interp.compute()
-            if interp.idx.point is None:
-                self.point = None
-                return self.point
-            q_interp = Variables(interp)
-            q_interp.compute_velocity()
-            v1 = q_interp.velocity.reshape(3)
-            k1 = time_step * v1
+            k1 = _rk4_step(self, x1)
+            if k1 is None: return None
             x2 = x0 + 0.5 * k1
 
-            idx = Search(self.interp.idx.grid, x2)
-            idx.compute()
-            interp = Interpolation(self.interp.flow, idx)
-            interp.compute()
-            if interp.idx.point is None:
-                self.point = None
-                return self.point
-            q_interp = Variables(interp)
-            q_interp.compute_velocity()
-            v2 = q_interp.velocity.reshape(3)
-            k2 = time_step * v2
+            k2 = _rk4_step(self, x2)
+            if k2 is None: return None
             x3 = x0 + k2
 
-            idx = Search(self.interp.idx.grid, x3)
-            idx.compute()
-            interp = Interpolation(self.interp.flow, idx)
-            interp.compute()
-            if interp.idx.point is None:
-                self.point = None
-                return self.point
-            q_interp = Variables(interp)
-            q_interp.compute_velocity()
-            v3 = q_interp.velocity.reshape(3)
-            k3 = time_step * v3
+            k3 = _rk4_step(self, x3)
+            if k3 is None: return None
             x4 = x0 + 1/6 * (k0 + 2*k1 + 2*k2 + k3)
 
-            self.point = x4
+            self.ppoint = x4
 
-            return self.point
+            return self.ppoint
 
         if method == 'cRK4':
             '''
@@ -113,8 +99,28 @@ class Integration:
             from src.streamlines.interpolation import Interpolation
             from src.streamlines.search import Search
 
-            x0 = self.interp.idx.point
+            def _rk4_step(self, x):
+                idx = Search(self.interp.idx.grid, x)
+                idx.block = self.interp.idx.block
+                idx.c2p(x)  # This will change the cell attribute
+                # In-domain check is done in search
+                if idx.cpoint is None:
+                    self.cpoint = None
+                    self.ppoint = None
+                    return None
+                interp = Interpolation(self.interp.flow, idx)
+                interp.compute(method='c-space')
+                _J_inv = interp.J_inv
+                q_interp = Variables(interp)
+                q_interp.compute_velocity()
+                p_velocity = q_interp.velocity.reshape(3)
+                c_velocity = np.matmul(_J_inv, p_velocity)
+                k = time_step * c_velocity
+                return k
+
+            x0 = self.interp.idx.cpoint
             _J_inv = self.interp.J_inv
+            if x0 is None: return None
 
             q_interp = Variables(self.interp)
             q_interp.compute_velocity()
@@ -122,81 +128,19 @@ class Integration:
             c_velocity = np.matmul(_J_inv, p_velocity)
             k0 = time_step * c_velocity
             x1 = x0 + 0.5 * k0
-            # For c-space the point in-domain check is done after integration
-            if not np.all([0, 0, 0] <= x1) or not np.all(
-                    x1 + 1 < [self.interp.idx.grid.ni[self.interp.idx.block],
-                              self.interp.idx.grid.nj[self.interp.idx.block],
-                              self.interp.idx.grid.nk[self.interp.idx.block]]):
-                self.point = None
-                print('Cannot run integration. The given point is out of domain\n')
-                return self.point
 
-            idx = Search(self.interp.idx.grid, x1)
-            idx.block = self.interp.idx.block
-            idx.c2p(x1)  # This will change the cell attribute
-            interp = Interpolation(self.interp.flow, idx)
-            interp.compute(method='c-space')
-            _J_inv = interp.J_inv
-
-            q_interp = Variables(interp)
-            q_interp.compute_velocity()
-            p_velocity = q_interp.velocity.reshape(3)
-            c_velocity = np.matmul(_J_inv, p_velocity)
-            k1 = time_step * c_velocity
+            k1 = _rk4_step(self, x1)
+            if k1 is None: return None
             x2 = x0 + 0.5 * k1
-            # For c-space the point in-domain check is done after integration
-            if not np.all([0, 0, 0] <= x2) or not np.all(
-                    x2 + 1 < [self.interp.idx.grid.ni[idx.block],
-                              self.interp.idx.grid.nj[idx.block],
-                              self.interp.idx.grid.nk[idx.block]]):
-                self.point = None
-                print('Cannot run integration. The given point is out of domain\n')
-                return self.point
 
-            idx = Search(self.interp.idx.grid, x2)
-            idx.block = self.interp.idx.block
-            idx.c2p(x2)  # This will change the cell attribute
-            interp = Interpolation(self.interp.flow, idx)
-            interp.compute(method='c-space')
-            _J_inv = interp.J_inv
-
-            q_interp = Variables(interp)
-            q_interp.compute_velocity()
-            p_velocity = q_interp.velocity.reshape(3)
-            c_velocity = np.matmul(_J_inv, p_velocity)
-            k2 = time_step * c_velocity
+            k2 = _rk4_step(self, x2)
+            if k2 is None: return None
             x3 = x0 + k2
-            # For c-space the point in-domain check is done after integration
-            if not np.all([0, 0, 0] <= x3) or not np.all(
-                    x3 + 1 < [self.interp.idx.grid.ni[idx.block],
-                              self.interp.idx.grid.nj[idx.block],
-                              self.interp.idx.grid.nk[idx.block]]):
-                self.point = None
-                print('Cannot run integration. The given point is out of domain\n')
-                return self.point
 
-            idx = Search(self.interp.idx.grid, x3)
-            idx.block = self.interp.idx.block
-            idx.c2p(x3)  # This will change cell attribute
-            interp = Interpolation(self.interp.flow, idx)
-            interp.compute(method='c-space')
-            _J_inv = interp.J_inv
-
-            q_interp = Variables(interp)
-            q_interp.compute_velocity()
-            p_velocity = q_interp.velocity.reshape(3)
-            c_velocity = np.matmul(_J_inv, p_velocity)
-            k3 = time_step * c_velocity
+            k3 = _rk4_step(self, x3)
+            if k3 is None: return None
             x4 = x0 + 1/6 * (k0 + 2*k1 + 2*k2 + k3)
-            # For c-spce the point in-domain check is done after integration
-            if not np.all([0, 0, 0] <= x4) or not np.all(
-                    x4 + 1 < [self.interp.idx.grid.ni[idx.block],
-                              self.interp.idx.grid.nj[idx.block],
-                              self.interp.idx.grid.nk[idx.block]]):
-                self.point = None
-                print('Cannot run integration. The given point is out of domain\n')
-                return self.point
 
-            self.point = x4
+            self.cpoint = x4
 
-            return self.point
+            return self.cpoint
