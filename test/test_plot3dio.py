3f9ee4c7ca390352340d4cc80a14d73490f63b83
kal <dilipkalagotla@gmail.com>
2022-01-21 13:20:07 -0500

    Added comments for better user readability in plot3dio. Added a unittest case to check for jacobians and inverse Jacobians

--- test/test_plot3dio.py
+++ test/test_plot3dio.py
@@ -3,6 +3,7 @@
 
 import unittest
 import doctest
+import numpy as np
 
 
 class TestIO(unittest.TestCase):
@@ -58,13 +59,86 @@ class TestIO(unittest.TestCase):
         from src.io.plot3dio import GridIO
 
         # Testing with a multi-block plate grid
-        grid = GridIO('../data/multi_block/plate/plate.mb.dp.x')
+        grid = GridIO('../data/multi_block/plate/plate.mb.sp.x')
         # Print the doc string
         print(grid)
-        grid.read_grid(data_type='f8')
+        grid.read_grid(data_type='f4')
+
+        # Compute metrics from the test setup function
+        test_grid = grid_metrics(grid)
 
         # Simple assertion to test the import
         self.assertEqual(grid.grd.shape, (grid.ni.max(), grid.nj.max(), grid.nk.max(), 3, grid.nb))
+        self.assertEqual(np.all(grid.m1 == test_grid.m1), True)
+        self.assertEqual(np.all(grid.m2 == test_grid.m2), True)
+        self.assertEqual(np.all(grid.J == test_grid.J), True)
+
+
+def grid_metrics(grid):
+    """
+    Computes grid metrics traditionally
+    Returns:
+         m1, m2, J
+    """
+
+    grid.m1 = np.zeros((grid.ni.max(), grid.nj.max(), grid.nk.max(), 3, 3, grid.nb))
+    grid.m2 = np.zeros((grid.ni.max(), grid.nj.max(), grid.nk.max(), 3, 3, grid.nb))
+    grid.J = np.zeros((grid.ni.max(), grid.nj.max(), grid.nk.max(), grid.nb))
+
+    # xi derivatives
+    for b in range(grid.nb):
+        for k in range(grid.nk[b]):
+            for j in range(grid.nj[b]):
+                grid.m1[0, j, k, 0, 0, b] = grid.grd[1, j, k, 0, b] - grid.grd[0, j, k, 0, b]
+                grid.m1[0, j, k, 1, 0, b] = grid.grd[1, j, k, 1, b] - grid.grd[0, j, k, 1, b]
+                grid.m1[0, j, k, 2, 0, b] = grid.grd[1, j, k, 2, b] - grid.grd[0, j, k, 2, b]
+                for i in range(1, grid.ni[b] - 1):
+                    grid.m1[i, j, k, 0, 0, b] = 0.5 * (grid.grd[i + 1, j, k, 0, b] - grid.grd[i - 1, j, k, 0, b])
+                    grid.m1[i, j, k, 1, 0, b] = 0.5 * (grid.grd[i + 1, j, k, 1, b] - grid.grd[i - 1, j, k, 1, b])
+                    grid.m1[i, j, k, 2, 0, b] = 0.5 * (grid.grd[i + 1, j, k, 2, b] - grid.grd[i - 1, j, k, 2, b])
+                grid.m1[grid.ni[b] - 1, j, k, 0, 0, b] = grid.grd[grid.ni[b] - 1, j, k, 0, b] - grid.grd[
+                    grid.ni[b] - 2, j, k, 0, b]
+                grid.m1[grid.ni[b] - 1, j, k, 1, 0, b] = grid.grd[grid.ni[b] - 1, j, k, 1, b] - grid.grd[
+                    grid.ni[b] - 2, j, k, 1, b]
+                grid.m1[grid.ni[b] - 1, j, k, 2, 0, b] = grid.grd[grid.ni[b] - 1, j, k, 2, b] - grid.grd[
+                    grid.ni[b] - 2, j, k, 2, b]
+        print('xi derivatives computed')
+        # eta derivatives
+        for k in range(grid.nk[b]):
+            for i in range(grid.ni[b]):
+                grid.m1[i, 0, k, 0, 1, b] = grid.grd[i, 1, k, 0, b] - grid.grd[i, 0, k, 0, b]
+                grid.m1[i, 0, k, 1, 1, b] = grid.grd[i, 1, k, 1, b] - grid.grd[i, 0, k, 1, b]
+                grid.m1[i, 0, k, 2, 1, b] = grid.grd[i, 1, k, 2, b] - grid.grd[i, 0, k, 2, b]
+                for j in range(1, grid.nj[b] - 1):
+                    grid.m1[i, j, k, 0, 1, b] = 0.5 * (grid.grd[i, j + 1, k, 0, b] - grid.grd[i, j - 1, k, 0, b])
+                    grid.m1[i, j, k, 1, 1, b] = 0.5 * (grid.grd[i, j + 1, k, 1, b] - grid.grd[i, j - 1, k, 1, b])
+                    grid.m1[i, j, k, 2, 1, b] = 0.5 * (grid.grd[i, j + 1, k, 2, b] - grid.grd[i, j - 1, k, 2, b])
+                grid.m1[i, grid.nj[b] - 1, k, 0, 1, b] = grid.grd[i, grid.nj[b] - 1, k, 0, b] - grid.grd[
+                    i, grid.nj[b] - 2, k, 0, b]
+                grid.m1[i, grid.nj[b] - 1, k, 1, 1, b] = grid.grd[i, grid.nj[b] - 1, k, 1, b] - grid.grd[
+                    i, grid.nj[b] - 2, k, 1, b]
+                grid.m1[i, grid.nj[b] - 1, k, 2, 1, b] = grid.grd[i, grid.nj[b] - 1, k, 2, b] - grid.grd[
+                    i, grid.nj[b] - 2, k, 2, b]
+        print('eta derivatives computed')
+        # zeta derivatives
+        for j in range(grid.nj[b]):
+            for i in range(grid.ni[b]):
+                grid.m1[i, j, 0, 0, 2, b] = grid.grd[i, j, 1, 0, b] - grid.grd[i, j, 0, 0, b]
+                grid.m1[i, j, 0, 1, 2, b] = grid.grd[i, j, 1, 1, b] - grid.grd[i, j, 0, 1, b]
+                grid.m1[i, j, 0, 2, 2, b] = grid.grd[i, j, 1, 2, b] - grid.grd[i, j, 0, 2, b]
+                for k in range(1, grid.nk[b] - 1):
+                    grid.m1[i, j, k, 0, 2, b] = 0.5 * (grid.grd[i, j, k + 1, 0, b] - grid.grd[i, j, k - 1, 0, b])
+                    grid.m1[i, j, k, 1, 2, b] = 0.5 * (grid.grd[i, j, k + 1, 1, b] - grid.grd[i, j, k - 1, 1, b])
+                    grid.m1[i, j, k, 2, 2, b] = 0.5 * (grid.grd[i, j, k + 1, 2, b] - grid.grd[i, j, k - 1, 2, b])
+                grid.m1[i, j, grid.nk[b] - 1, 0, 2, b] = grid.grd[i, j, grid.nk[b] - 1, 0, b] - grid.grd[
+                    i, j, grid.nk[b] - 2, 0, b]
+                grid.m1[i, j, grid.nk[b] - 1, 1, 2, b] = grid.grd[i, j, grid.nk[b] - 1, 1, b] - grid.grd[
+                    i, j, grid.nk[b] - 2, 1, b]
+                grid.m1[i, j, grid.nk[b] - 1, 2, 2, b] = grid.grd[i, j, grid.nk[b] - 1, 2, b] - grid.grd[
+                    i, j, grid.nk[b] - 2, 2, b]
+        print('zeta derivatives')
+
+        return grid
 
 
 if __name__ == '__main__':
