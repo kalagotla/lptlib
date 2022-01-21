dfaf286f6c94d04f95a83110468e2e64f41e93f3
kal <dilipkalagotla@gmail.com>
2022-01-21 13:20:45 -0500

    Added a test case to check for correctness of newton-raphson algo

--- test/test_search.py
+++ test/test_search.py
@@ -13,10 +13,10 @@ class TestSearch(unittest.TestCase):
     # sp: single precision; dp: double precision
     # Add more cases as a tuple below
     @parameterized.expand([
-        # ('sb_sp_distance', '../data/plate_data/plate.sp.x', 'f4', 'distance'),
-        # ('sb_sp_block_distance', '../data/plate_data/plate.sp.x', 'f4', 'block_distance'),
-        # ('mb_dp_distance', '../data/multi_block/plate/plate.mb.dp.x', 'f8', 'distance'),
-        # ('mb_dp_block_distance', '../data/multi_block/plate/plate.mb.dp.x', 'f8', 'block_distance'),
+        ('sb_sp_distance', '../data/plate_data/plate.sp.x', 'f4', 'distance'),
+        ('sb_sp_block_distance', '../data/plate_data/plate.sp.x', 'f4', 'block_distance'),
+        ('mb_dp_distance', '../data/multi_block/plate/plate.mb.dp.x', 'f8', 'distance'),
+        ('mb_dp_block_distance', '../data/multi_block/plate/plate.mb.dp.x', 'f8', 'block_distance'),
         ('mb_dp_block_distance', '../data/multi_block/plate/plate.mb.dp.x', 'f8', 'c-space')
 
     ])
@@ -47,7 +47,7 @@ class TestSearch(unittest.TestCase):
         # Check for on the node case
         # Used a Node from grid numbering
         idx = Search(grid, grid.grd[0, 0, 0, :, 0])
-        idx.compute(method=method)
+        idx.compute(method='block_distance')
         self.assertEqual(idx.info, 'Given point is a node in the domain with a tol of 1e-6.\n'
                                    'Interpolation will assign node properties for integration.\n'
                                    'Index of the node will be returned by cell attribute\n')
@@ -57,6 +57,22 @@ class TestSearch(unittest.TestCase):
         idx.compute(method=method)
         self.assertEqual(idx.cell.shape, (8, 3))
         self.assertEqual(idx.info, None)
+
+    def test_p2c(self, name='p2c_test', filename='../data/plate_data/plate.sp.x', data_type='f4'):
+        from src.io.plot3dio import GridIO
+        from src.streamlines.search import Search
+
+        # Read the grid data
+        grid = GridIO(filename)
+        grid.read_grid(data_type=data_type)
+        grid.compute_metrics()
+        flow = None
+
+        # Start test for search algorithm
+        # Check if distance and neighbors are working fine
+        point = [8.5, 0.5, 0.01]
+        idx = Search(grid, point)
+        idx.p2c(point)
 
 
 if __name__ == '__main__':
