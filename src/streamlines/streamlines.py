392444ac2f58048855c96d429fc6c2245b2fa48a
kal <dilipkalagotla@gmail.com>
2022-03-29 20:34:10 +0530

    Updated comments! Created vortex test case using an analytical flow

--- src/streamlines/streamlines.py
+++ src/streamlines/streamlines.py
@@ -1,17 +1,59 @@
 # Uses the program API to extract streamlines
+import numpy as np
+
 
 class Streamlines:
+    """
+    Module to extract particle path information
+
+    ...
+
+    Attributes
+    ----------
+    Input:
+        grid_file : str
+            Path to the plot3d grid data file
+        flow_file : str
+            Path to the plot3d flow data file
+        point : list
+            Starting point for integration
+        search_method : str
+            Default is p-space; can specify c-space
+        interpolation_method : str
+            Default is p-space; can specify c-space
+        integration_method : str
+            Default is p-space; can specify c-space
+        time_step : float
+            Default is 1e-3
+
+    Output:
+        streamline : numpy.ndarray
+            shape is nx3; each column represents x, y, z
+
+    Methods
+    -------
+    compute()
+        integrates and returns the streamline ndarray
+    ...
+
+    Example:
+        sl = Streamlines('../../data/vortex/vortex.sb.sp.x', '../../data/vortex/vortex.sb.sp.q', [-0.05, 0.05, 5])
+        sl.compute()
+    -------
+
+
+    """
     def __init__(self, grid_file, flow_file, point,
                  search_method='p-space', interpolation_method='p-space', integration_method='p-space',
                  time_step=1e-3):
         self.grid_file = grid_file
         self.flow_file = flow_file
-        self.point = point
+        self.point = np.array(point)
         self.search_method = search_method
         self.interpolation_method = interpolation_method
         self.integration_method = integration_method
         self.time_step = time_step
-        self.streamline = point
+        self.streamline = []
 
     def compute(self):
         from src.function.timer import Timer
@@ -29,6 +71,7 @@ class Streamlines:
         flow.read_flow()
         grid.compute_metrics()
 
+        self.streamline.append(self.point)
         while True:
             with Timer(text="Elapsed time for loop number " + str(len(self.streamline)) + ": {:.8f}"):
                 idx = Search(grid, self.point)
---
+++ test/streamlines/test_vortex.py
@@ -0,0 +1,32 @@
+import unittest
+import numpy as np
+
+
+class TestVortex(unittest.TestCase):
+    def test_vortex(self):
+        """
+        Applies streamlines algo for vortex field.
+        Details of the vortex can be found in Murman and Powell, 1987
+
+        Returns:
+        """
+        from src.streamlines.streamlines import Streamlines
+        sl = Streamlines('../../data/vortex/vortex.sb.sp.x', '../../data/vortex/vortex.sb.sp.q', [-0.05, 0.05, 5])
+        sl.compute()
+
+        import matplotlib.pyplot as plt
+        data = np.array(sl.streamline)
+        xp, yp, zp = data[:, 0], data[:, 1], data[:, 2]
+
+        ax = plt.axes(projection='3d')
+        ax.plot3D(xp, yp, zp, 'b', label='SB-P')
+        ax.set_title('Comparing different particle path algorithms')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.legend()
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
