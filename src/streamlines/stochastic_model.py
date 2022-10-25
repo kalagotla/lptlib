6d23bd654def40afc2fe3fac7a404360e260816f
kal <dilipkalagotla@gmail.com>
2022-10-25 17:06:34 -0400

    Stochastic model implemented and tested. Streamlines modified for better parallel implementation

---
+++ src/streamlines/stochastic_model.py
@@ -0,0 +1,139 @@
+# Class to run streamlines script in parallel
+# Stochastic model for tracers is implemented
+
+import numpy as np
+import matplotlib.pyplot as plt
+from multiprocessing.pool import ThreadPool as Pool
+import multiprocessing as mp
+from src.io.plot3dio import GridIO, FlowIO
+from src.streamlines.streamlines import Streamlines
+
+rng = np.random.default_rng(7)
+
+
+class StochasticModel(Streamlines):
+    """Module to spawn and run LPT on given tracers parallely
+
+    ...
+
+    Attributes
+    ----------
+
+    Input:
+
+    """
+
+    def __init__(self, particles, spawn_locations, method='adaptive-p-space',
+                 grid_file=None, flow_file=None, point=None,
+                 search='block_distance', interpolation='p-space', integration='pRK4',
+                 diameter=1e-7, density=1000, viscosity=1.827e-5,
+                 time_step=1e-3, max_time_step=1, drag_model='stokes'
+                 ):
+        super().__init__(grid_file=grid_file, flow_file=flow_file, point=point,
+                         search=search, interpolation=interpolation, integration=integration,
+                         diameter=diameter, density=density, viscosity=viscosity,
+                         time_step=time_step, max_time_step=max_time_step, drag_model=drag_model)
+        self.particles = particles
+        self.spawn_locations = spawn_locations
+        # Read-in grid and flow files
+        self.grid = GridIO(self.grid_file)
+        self.flow = FlowIO(self.flow_file)
+        self.method = method
+        self.grid.read_grid()
+        self.grid.compute_metrics()
+        self.flow.read_flow()
+
+    def setup(self, spawn_location, particle_dia):
+        """
+        Sets up the function to be run in parallel
+        Args:
+            self:
+            spawn_location:
+            particle_dia:
+
+        Returns:
+
+        """
+        # TODO: Have to use inheritance properties. Currently, just calling in another object
+        sl = Streamlines(None, None, point=spawn_location, diameter=particle_dia, time_step=self.time_step)
+        sl.density = self.particles.density
+        sl.drag_model = self.drag_model
+        sl.max_time_step = self.max_time_step
+        sl.compute(method=self.method, grid=self.grid, flow=self.flow)
+
+        return sl
+
+    def multi_process(self):
+        """
+        To parallelize the setup function
+        Returns:
+
+        """
+        pool = Pool(mp.cpu_count() - 1)
+        lpt_data = pool.starmap(self.setup, zip(self.spawn_locations.locations, self.particles.particle_field))
+
+        return lpt_data
+
+
+class Particle:
+    """
+    Class holds details for particles used in a PIV experiment
+    ---
+    User has to provide all the information to generate size distribution
+    """
+
+    def __init__(self):
+        self.distribution = "gaussian"
+        self.min_dia = None
+        self.max_dia = None
+        self.mean_dia = None
+        self.std_dia = None
+        self.density = None
+        self.n_concentration = None
+        self.particle_field = None
+
+    def compute_distribution(self):
+        """
+        Run this method to return a distribution of particle diameters
+        :return: numpy.ndarray
+        A 1d array of particle diameters
+        """
+        if self.distribution == "gaussian":
+            print("When Gaussian distribution is used,"
+                  " the particle statistics are computed using mean and std diameters\n"
+                  "Particle min and max are cutoffs for the distribution")
+            self.particle_field = rng.normal(self.mean_dia, self.std_dia, int(self.n_concentration))
+            self.particle_field = np.clip(self.particle_field, self.min_dia, self.max_dia)
+            np.random.shuffle(self.particle_field)
+            return
+
+        # TODO: Add Uniform distribution
+
+    pass
+
+
+class SpawnLocations:
+    """
+    Creates spawn locations array based on number of particles
+    """
+    def __init__(self, particles):
+        self.x_min, self.x_max = None, None
+        self.y_min, self.y_max = None, None
+        self.z_min, self.z_max = None, None
+        self.particles = particles
+        self.locations = None
+
+    def compute(self):
+        """
+        Computes the locations array to be passed into parallel
+        Returns:
+
+        """
+        _size = self.particles.n_concentration
+        # Draw a straight line between given points
+        if self.x_max is None and self.z_max is None:
+            _x_temp = np.repeat(self.x_min, _size).reshape(_size, 1)
+            _z_temp = np.repeat(self.z_min, _size).reshape(_size, 1)
+            _y_temp = np.linspace(self.y_min, self.y_max, _size).reshape(_size, 1)
+
+            self.locations = np.hstack((_x_temp, _y_temp, _z_temp))
---
+++ test/test_stochastic_model.py
@@ -0,0 +1,61 @@
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+from src.streamlines.stochastic_model import StochasticModel, Particle, SpawnLocations
+
+
+class TestStochasticModel(unittest.TestCase):
+    def test_stochastic_model(self):
+        # Test particle class
+        p = Particle()
+        p.min_dia = 177e-9
+        p.max_dia = 573e-9
+        p.mean_dia = 281e-9
+        p.std_dia = 97e-9
+        p.density = 813
+        p.n_concentration = 25
+        p.compute_distribution()
+
+        # Test SpawnLocations class
+        l = SpawnLocations(p)
+        l.x_min = 9e-4
+        l.z_min = 2e-4
+        l.y_min, l.y_max = 2e-4, 15e-4
+        l.compute()
+
+        # Run the model in parallel
+        grid_file, flow_file = '../data/shocks/shock_test.sb.sp.x', '../data/shocks/shock_test.sb.sp.q'
+        sm = StochasticModel(p, l, grid_file=grid_file, flow_file=flow_file)
+        sm.method = 'adaptive-ppath'
+        sm.time_step = 1e-8
+        sm.max_time_step = 1e-9
+
+        # Run multi-process
+        lpt_data = sm.multi_process()
+
+        # sl = sm.setup([18e-4, 2e-4, 2e-4], p.mean_dia)
+        #
+        # Plot data
+        ax = plt.axes()
+        for i in range(p.n_concentration):
+            xdata = np.array(lpt_data[i].streamline)
+            vdata = np.array(lpt_data[i].svelocity)
+            udata = np.array(lpt_data[i].fvelocity)
+            xp, yp, zp = xdata[:, 0], xdata[:, 1], xdata[:, 2]
+            vx, vy, vz = vdata[:, 0], vdata[:, 1], vdata[:, 2]
+            ux, uy, uz = udata[:, 0], udata[:, 1], udata[:, 2]
+
+            # ax.plot(xp, vx, 'r', label='Particle')
+            # ax.plot(xp, ux, 'b', label='Fluid')
+            ax.plot(xp, yp, '.-', label='Path')
+            # ax.set_title(name)
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_xlim(0, 38e-4)
+            # ax.legend()
+        ax.set_title(sm.method)
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
