Added rgi-c-space; corrected jacobian from rbf-c-space

src/streamlines/interpolation.py
@@ -399,9 +399,9 @@ class Interpolation:
                 self.q = self.q.reshape((1, 1, 1, -1, 1))
 
                 _rbf_J = rbf(_unit_cell, _cell_J, kernel=self.rbf_kernel)
-                self.J = _rbf_J(_fractions)
+                self.J = _rbf_J(_fractions).reshape(3, 3)
                 _rbf_J_inv = rbf(_unit_cell, _cell_J_inv, kernel=self.rbf_kernel)
-                self.J_inv = _rbf_J_inv(_fractions)
+                self.J_inv = _rbf_J_inv(_fractions).reshape(3, 3)
 
 
             case 'rgi-p-space':
@@ -411,7 +411,6 @@ class Interpolation:
                 # TODO: debug for multiblock case -- currently defaults to single block; if not enough cells in
                 #  the block, it defaults to the single cell case
                 if np.any(np.array(self.level) > 0):
-                    _level_cell = self.idx.cell
                     # add cells in i-direction
                     if np.any(self.idx.cell[:, 0] + self.level[0] >= self.idx.grid.ni - 1) or np.any(
                         self.idx.cell[:, 0] - self.level[0] <= 0
@@ -503,4 +502,157 @@ class Interpolation:
                                    _rgi_rho_v(self.idx.ppoint), _rgi_rho_w(self.idx.ppoint),
                                    _rgi_e(self.idx.ppoint)])
                 self.q = self.q.reshape((1, 1, 1, -1, 1))
+
+            case 'rgi-c-space':
+                # implement rgi interpolation in c-space
+                # if the point is node return node data
+                if self.idx.info == 'Given point is a node in the domain with a tol of 1e-6.\n' \
+                                    'Interpolation will assign node properties for integration.\n' \
+                                    'Index of the node will be returned by cell attribute\n':
+                    self.q = _cell_q
+                    self.q = self.q.reshape((1, 1, 1, -1, 1))
+                    return
+
+                # Interpolate m1 and m2 for use in integration; m1 is used in ppath only!
+                # _alpha, _beta, _gamma and reshape for rbf intepolator to work
+                _fractions = (self.idx.cpoint - self.idx.cell[0]).reshape(1, -1)
+
+                # add adjacent cells to the cell list
+                # TODO: debug for multiblock case -- currently defaults to single block
+                _unit_cell = np.array([[0, 0, 0], [1, 0, 0], [1, 1, 0],
+                                       [0, 1, 0], [0, 0, 1], [1, 0, 1],
+                                       [1, 1, 1], [0, 1, 1]])
+
+                # Add cells in _i, _j, _k directions
+                if np.any(np.array(self.level) > 0):
+                    # add cells in i-direction
+                    if np.any(self.idx.cell[:, 0] + self.level[0] >= self.idx.grid.ni - 1) or np.any(
+                        self.idx.cell[:, 0] - self.level[0] <= 0
+                    ):
+                        _i_cell = np.unique(self.idx.cell[:, 0])
+                        _i_unit_cell = _unit_cell[:, 0]
+                        pass
+                    else:
+                        _i_cell = np.unique(self.idx.cell[:, 0])
+                        _i_unit_cell = _unit_cell[:, 0]
+                        for _i in range(self.level[0]):
+                            _i_cell = np.hstack((_i_cell, self.idx.cell[1, 0] + _i + 1))
+                            _i_cell = np.hstack((_i_cell, self.idx.cell[0, 0] - _i - 1))
+                            _i_unit_cell = np.hstack((_i_unit_cell, _unit_cell[:, 0] + _i + 1))
+                            _i_unit_cell = np.hstack((_i_unit_cell, _unit_cell[:, 0] - _i - 1))
+                    # add nodes in j-direction
+                    if np.any(self.idx.cell[:, 1] + self.level[1] >= self.idx.grid.nj - 1) or np.any(
+                            self.idx.cell[:, 1] - self.level[1] <= 0
+                    ):
+                        _j_cell = np.unique(self.idx.cell[:, 1])
+                        _j_unit_cell = _unit_cell[:, 1]
+                        pass
+                    else:
+                        _j_cell = np.unique(self.idx.cell[:, 1])
+                        _j_unit_cell = _unit_cell[:, 1]
+                        for _i in range(self.level[1]):
+                            _j_cell = np.hstack((_j_cell, self.idx.cell[2, 1] + _i + 1))
+                            _j_cell = np.hstack((_j_cell, self.idx.cell[0, 1] - _i - 1))
+                            _j_unit_cell = np.hstack((_j_unit_cell, _unit_cell[:, 1] + _i + 1))
+                            _j_unit_cell = np.hstack((_j_unit_cell, _unit_cell[:, 1] - _i - 1))
+                    # add nodes in k-direction
+                    if np.any(self.idx.cell[:, 2] + self.level[2] >= self.idx.grid.nk - 1) or np.any(
+                            self.idx.cell[:, 2] - self.level[2] <= 0
+                    ):
+                        _k_cell = np.unique(self.idx.cell[:, 2])
+                        _k_unit_cell = _unit_cell[:, 2]
+                        pass
+                    else:
+                        _k_cell = np.unique(self.idx.cell[:, 2])
+                        _k_unit_cell = _unit_cell[:, 2]
+                        for _i in range(self.level[2]):
+                            _k_cell = np.hstack((_k_cell, self.idx.cell[4, 2] + _i + 1))
+                            _k_cell = np.hstack((_k_cell, self.idx.cell[0, 2] - _i - 1))
+                            _k_unit_cell = np.hstack((_k_unit_cell, _unit_cell[:, 2] + _i + 1))
+                            _k_unit_cell = np.hstack((_k_unit_cell, _unit_cell[:, 2] - _i - 1))
+                    # create a meshgrid of all possible combinations
+                    _level_cell = np.meshgrid(np.sort(_i_cell), np.sort(_j_cell), np.sort(_k_cell))
+                    _level_unit_cell = np.meshgrid(np.sort(_i_unit_cell), np.sort(_j_unit_cell), np.sort(_k_unit_cell))
+                    # re-order back to an array with coordinates
+                    _level_cell = np.moveaxis(np.array(_level_cell), 0, _level_cell[0].ndim).reshape(-1, len(_level_cell))
+                    _level_cell = np.unique(_level_cell, axis=0)
+                    _level_unit_cell = np.moveaxis(np.array(_level_unit_cell), 0, _level_unit_cell[0].ndim).reshape(-1, len(_level_unit_cell))
+                    _level_unit_cell = np.unique(_level_unit_cell, axis=0)
+                # if all zeros; use the cell
+                elif np.all(np.array(self.level) == 0):
+                    _level_cell = np.unique(self.idx.cell, axis=0)
+                    _level_unit_cell = _unit_cell
+
+                # assign cell values
+                _cell_grd = self.idx.grid.grd[
+                            _level_cell[:, 0], _level_cell[:, 1], _level_cell[:, 2], :, self.idx.block]
+                _cell_q = self.flow.q[
+                            _level_cell[:, 0], _level_cell[:, 1], _level_cell[:, 2], :, self.idx.block]
+                _cell_J = self.idx.grid.m1[
+                          _level_cell[:, 0], _level_cell[:, 1], _level_cell[:, 2], :, :, self.idx.block]
+                _cell_J_inv = self.idx.grid.m2[
+                            _level_cell[:, 0], _level_cell[:, 1], _level_cell[:, 2], :, :, self.idx.block]
+
+                # Start the RGI
+                from scipy.interpolate import RegularGridInterpolator
+                # Find the unique values in each direction
+                _x = np.unique(_level_unit_cell[:, 0])
+                _y = np.unique(_level_unit_cell[:, 1])
+                _z = np.unique(_level_unit_cell[:, 2])
+                # Set the shape for reshaping q
+                _shape = np.array([len(_x), len(_y), len(_z)])
+
+                # Depending on the shape set the best possible interpolation method
+                if self.rgi_method == 'adaptive':
+                    if np.all(_shape >= 6):
+                        _method = 'quintic'
+                    elif np.all(_shape >= 4):
+                        _method = 'cubic'
+                    else:
+                        _method = 'linear'
+                else:
+                    _method = self.rgi_method
+
+                # Create the RGI for each variable
+                _rgi_rho = RegularGridInterpolator((_x, _y, _z), _cell_q[:, 0].reshape(_shape), method=_method)
+                _rgi_rho_u = RegularGridInterpolator((_x, _y, _z), _cell_q[:, 1].reshape(_shape), method=_method)
+                _rgi_rho_v = RegularGridInterpolator((_x, _y, _z), _cell_q[:, 2].reshape(_shape), method=_method)
+                _rgi_rho_w = RegularGridInterpolator((_x, _y, _z), _cell_q[:, 3].reshape(_shape), method=_method)
+                _rgi_e = RegularGridInterpolator((_x, _y, _z), _cell_q[:, 4].reshape(_shape), method=_method)
+
+                # Find the values at the point and reshape
+                self.q = np.array([_rgi_rho(_fractions), _rgi_rho_u(_fractions),
+                                      _rgi_rho_v(_fractions), _rgi_rho_w(_fractions),
+                                      _rgi_e(_fractions)])
+                self.q = self.q.reshape((1, 1, 1, -1, 1))
+
+                _rgi_J00 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 0, 0].reshape(_shape), method=_method)
+                _rgi_J01 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 0, 1].reshape(_shape), method=_method)
+                _rgi_J02 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 0, 2].reshape(_shape), method=_method)
+                _rgi_J10 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 1, 0].reshape(_shape), method=_method)
+                _rgi_J11 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 1, 1].reshape(_shape), method=_method)
+                _rgi_J12 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 1, 2].reshape(_shape), method=_method)
+                _rgi_J20 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 2, 0].reshape(_shape), method=_method)
+                _rgi_J21 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 2, 1].reshape(_shape), method=_method)
+                _rgi_J22 = RegularGridInterpolator((_x, _y, _z), _cell_J[:, 2, 2].reshape(_shape), method=_method)
+                _J00, _J01, _J02 = _rgi_J00(_fractions), _rgi_J01(_fractions), _rgi_J02(_fractions)
+                _J10, _J11, _J12 = _rgi_J10(_fractions), _rgi_J11(_fractions), _rgi_J12(_fractions)
+                _J20, _J21, _J22 = _rgi_J20(_fractions), _rgi_J21(_fractions), _rgi_J22(_fractions)
+                self.J = np.array([[_J00, _J01, _J02], [_J10, _J11, _J12], [_J20, _J21, _J22]]).reshape(3, 3)
+
+                _rgi_J00_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 0, 0].reshape(_shape), method=_method)
+                _rgi_J01_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 0, 1].reshape(_shape), method=_method)
+                _rgi_J02_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 0, 2].reshape(_shape), method=_method)
+                _rgi_J10_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 1, 0].reshape(_shape), method=_method)
+                _rgi_J11_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 1, 1].reshape(_shape), method=_method)
+                _rgi_J12_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 1, 2].reshape(_shape), method=_method)
+                _rgi_J20_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 2, 0].reshape(_shape), method=_method)
+                _rgi_J21_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 2, 1].reshape(_shape), method=_method)
+                _rgi_J22_inv = RegularGridInterpolator((_x, _y, _z), _cell_J_inv[:, 2, 2].reshape(_shape), method=_method)
+                _J00_inv, _J01_inv, _J02_inv = _rgi_J00_inv(_fractions), _rgi_J01_inv(_fractions), _rgi_J02_inv(_fractions)
+                _J10_inv, _J11_inv, _J12_inv = _rgi_J10_inv(_fractions), _rgi_J11_inv(_fractions), _rgi_J12_inv(_fractions)
+                _J20_inv, _J21_inv, _J22_inv = _rgi_J20_inv(_fractions), _rgi_J21_inv(_fractions), _rgi_J22_inv(_fractions)
+                self.J_inv = np.array([[_J00_inv, _J01_inv, _J02_inv],
+                                      [_J10_inv, _J11_inv, _J12_inv],
+                                      [_J20_inv, _J21_inv, _J22_inv]]).reshape(3, 3)
 