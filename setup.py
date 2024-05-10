88a8529f13db19f5d86ff7d3434f40b820fb5b22
kal <dilipkalagotla@gmail.com>
2024-05-10 16:56:44 -0400

    Publishing to PyPI as v.0.1a

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1"
-DESCRIPTION = "A Lagrangian Particle Tracking package"
-LONG_DESCRIPTION = "Includes a set of tools for Lagrangian Particle Tracking like search, interpolation, etc."
+VERSION = "0.1a"
+DESCRIPTION = "One-way coupled Lagrangian Particle Tracking for CFD simulations. Includes many auxiliary tools."
+LONG_DESCRIPTION = "Detailed description of the package can be requested from the author at this moment."
 
 # Setting up
 setup(
     # name must match the folder name
     name="project-arrakis",
     version=VERSION,
+    url="https://github.com/kalagotla/project-arrakis",
+    license="MIT",
     author="kal @ Dilip Kalagotla",
     author_email="<dilipkalagotla@gmail.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],  # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
+    install_requires=[],
     keywords=["python", "first package"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
