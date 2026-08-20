f0bef29b59041551ea22f95c464322871aa3255d
Dilip <41090393+kalagotla@users.noreply.github.com>
2026-08-20 16:16:59 -0400

    Refactor test_dataio to use require_data for paths

--- test/test_dataio.py
+++ test/test_dataio.py
@@ -3,21 +3,23 @@ import unittest
 
 class TestDataIO(unittest.TestCase):
     def test_dataio(self):
-        from src.lptlib.io import DataIO
-        from src.lptlib.io import GridIO, FlowIO
+        from lptlib.io import DataIO
+        from lptlib.io import GridIO, FlowIO
+        from testdata import require_data
         # grid object
-        grid = GridIO('../data/shocks/shock_test.sb.sp.x')
+        grid = GridIO(require_data('shocks', 'shock_test.sb.sp.x'))
         grid.read_grid()
         grid.compute_metrics()
 
         # flow object
-        flow = FlowIO('../data/shocks/shock_test.sb.sp.q')
+        flow = FlowIO(require_data('shocks', 'shock_test.sb.sp.q'))
         flow.read_flow()
 
         # data module test
-        # data = DataIO(grid, flow, location='../data/shocks/particle_data/multi_process_test/')
-        data = DataIO(grid, flow, location='../data/shocks/particle_data/281nm_time_step_adaptive/old_data/',
-                      read_file='../data/shocks/particle_data/281nm_time_step_adaptive/old_data/combined_file.npy')
+        location = require_data('shocks', 'particle_data', '281nm_time_step_adaptive', 'old_data')
+        read_file = require_data('shocks', 'particle_data', '281nm_time_step_adaptive', 'old_data',
+                                 'combined_file.npy')
+        data = DataIO(grid, flow, location=location, read_file=read_file)
         data.percent_data = 0.1
         # Increased refinement for better resolution
         data.x_refinement = 500
