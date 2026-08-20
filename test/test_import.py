03bcf3b645a411dd4f3a54f5059f42632d63c7e6
Dilip <41090393+kalagotla@users.noreply.github.com>
2026-08-20 16:16:07 -0400

    Remove 'src' prefix from import statements

--- test/test_import.py
+++ test/test_import.py
@@ -3,15 +3,15 @@ import unittest
 
 class TestImport(unittest.TestCase):
     def test_import(self):
-        from src.lptlib.io import GridIO, FlowIO
+        from lptlib.io import GridIO, FlowIO
 
         grd = GridIO(filename='../data/plate_data/plate.sp.x')
         flw = FlowIO('../data/plate_data/sol-0000010.q')
 
-        import src.lptlib
+        import lptlib
 
-        grd = src.lptlib.GridIO(filename='../data/plate_data/plate.sp.x')
-        flw = src.lptlib.FlowIO('../data/plate_data/sol-0000010.q')
+        grd = lptlib.GridIO(filename='../data/plate_data/plate.sp.x')
+        flw = lptlib.FlowIO('../data/plate_data/sol-0000010.q')
 
 
 if __name__ == '__main__':
